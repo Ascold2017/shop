7726cc5f7764046267e8beaad0489dc6a3aa6afa
Ascold1 <ascold96@gmail.com>
2017-08-10 19:35:58 +0300

    first commit

--- js/common.js
+++ js/common.js
@@ -26,6 +26,38 @@ $(document).ready(function(){
         slidesToScroll: 1,
         arrows: true,
          nextArrow:'<button type="button" class="arrownext"><img src="img/arrownextblack.png" alt="next" /></button>',
+        responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            infinite: true,
+          },
+            
+        },
+        {
+            breakpoint: 720,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 450,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+      ]
     });
     $(".product_slider").slick({
         slidesToShow: 5,
